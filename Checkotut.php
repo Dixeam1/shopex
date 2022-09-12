b4df806e10e2f7ebeaa0c2f1d87f2f31cffb60e7
ZainJon06 <zainjob.74@devaims.com>
2022-09-12 20:05:28 +0500

    fsd

--- Checkotut.php
+++ Checkotut.php
@@ -1,155 +1,205 @@
-<?php
-include 'conn.php';
-include 'function.php';
-
-
-session_start();
-
-if (empty(isset($_SESSION["email"]))) {
-	header("Location:dist/login.php");
-}
-?>
 <!DOCTYPE html>
-<html lang="en" >
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>Order Now</title>
-	<link rel="stylesheet" href="vendors/css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+  <style>
+    body {
+      font-family: Arial;
+      font-size: 17px;
+      padding: 8px;
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+
+    .row {
+      display: -ms-flexbox;  /*IE10*/ 
+      display: flex;
+      justify-content: center !important;
+      /*-ms-flex-wrap: wrap;  IE10 */*/
+      /*flex-wrap: wrap;*/
+      /*margin: 0 -16px;*/
+      width: 60%;
+    }
+
+    .col-25 {
+      -ms-flex: 25%; /* IE10 */
+      flex: 25%;
+    }
+
+    .col-50 {
+      -ms-flex: 50%; /* IE10 */
+      flex: 50%;
+    }
+
+    .col-75 {
+      -ms-flex: 75%; /* IE10 */
+      flex: 75%;
+    }
+
+    .col-25,
+    .col-50,
+    .col-75 {
+      padding: 0 16px;
+    }
+
+    .container {
+      background-color: #f2f2f2;
+      padding: 5px 20px 15px 20px;
+      border: 1px solid lightgrey;
+      border-radius: 3px;
+    }
+
+    input[type=text] {
+      width: 100%;
+      margin-bottom: 20px;
+      padding: 12px;
+      border: 1px solid #ccc;
+      border-radius: 3px;
+    }
+
+    label {
+      margin-bottom: 10px;
+      display: block;
+    }
+
+    .icon-container {
+      margin-bottom: 20px;
+      padding: 7px 0;
+      font-size: 24px;
+    }
+
+    .btn {
+      background-color: #04AA6D;
+      color: white;
+      padding: 12px;
+      margin: 10px 0;
+      border: none;
+      width: 100%;
+      border-radius: 3px;
+      cursor: pointer;
+      font-size: 17px;
+    }
+
+    .btn:hover {
+      /*background-color: #45a049;*/
+    }
+
+    a {
+      color: #2196F3;
+    }
+
+    hr {
+      border: 1px solid lightgrey;
+    }
+
+    span.price {
+      float: right;
+      color: grey;
+    }
+    .cehck{
+      color: green;
+      text-align: center;
+      padding:30px 10px 50px;
+    }
+
+    /* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (also change the direction - make the "cart" column go on top) */
+    @media (max-width: 800px) {
+      .row {
+        flex-direction: column-reverse;
+      }
+      .col-25 {
+        margin-bottom: 20px;
+      }
+    }
+  </style>
 </head>
 <body>
+  <div class="cehck">
+      <h1>
+        Checkout
+      </h1>
+  </div>
 
-	<header>
-		<li style="list-style: none; float: left;">
-			<h3>Order Now</h3>
-		</li >
-		<a  href="dist/logout.php" class="list-style" style="list-style: none; float: right;">
-	
-			logout
-		</a>
-		
-	</header>
+  <div class="row">
+    <div class="col-75">
+      <div class="container">
+        <form action="" method="POST">
 
-	<main>
-		
-		<section class="checkout-form">
-			<form action="#!" method="get">
-				<h6>Contact information</h6>
-				<div class="form-control">
-					<label for="checkout-email">E-mail</label>
-					<div>
-						<span class="fa fa-envelope"></span>
-						<input type="email" id="checkout-email" name="checkout-email" placeholder="Enter your email...">
-					</div>
-				</div>
-				<div class="form-control">
-					<label for="checkout-phone">Phone</label>
-					<div>
-						<span class="fa fa-phone"></span>
-						<input type="tel" name="checkout-phone" id="checkout-phone" placeholder="Enter you phone...">
-					</div>
-				</div>
-				<br>
-				<h6>Shipping address</h6>
-				<div class="form-control">
-					<label for="checkout-name">Full name</label>
-					<div>
-						<span class="fa fa-user-circle"></span>
-						<input type="text" id="checkout-name" name="checkout-name" placeholder="Enter you name...">
-					</div>
-				</div>
-				<div class="form-control">
-					<label for="checkout-address">Address</label>
-					<div>
-						<span class="fa fa-home"></span>
-						<input type="text" name="checkout-address" id="checkout-address" placeholder="Your address...">
-					</div>
-				</div>
-				<div class="form-control">
-					<label for="checkout-city">City</label>
-					<div>
-						<span class="fa fa-building"></span>
-						<input type="text" name="checkout-city" id="checkout-city" placeholder="Your city...">
-					</div>
-				</div>
-				<div class="form-group">
-					<div class="form-control">
-						<label for="checkout-country">Country</label>
-						<div>
-							<span class="fa fa-globe"></span>
-							<input type="text" name="checkout-country" id="checkout-country" placeholder="Your country..." list="country-list">
-							<datalist id="country-list">
-								<option value="Pakistan"></option>
-								<option value="USA"></option>
-								<option value="Russia"></option>
-								<option value="Japan"></option>
-								<option value="Egypt"></option>
-							</datalist>
-						</div>
-					</div>
-					<div class="form-control">
-						<label for="checkout-postal">Postal code</label>
-						<div>
-							<span class="fa fa-archive"></span>
-							<input type="numeric" name="checkout-postal" id="checkout-postal" placeholder="Your postal code...">
-						</div>
-					</div>
-				</div>
-				
-				<div class="form-control-btn ">
-					<button>Order Now</button>
-				</div>
-			</form>
-		</section>
+          <div class="row">
+            <div class="col-50">
+              <h3>Billing Address</h3>
+              <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+              <input required  type="text" id="fname" name="firstname" placeholder="John M. Doe">
+              <label for="email"><i class="fa fa-envelope"></i> Email</label>
+              <input required  type="text" id="email" name="email" placeholder="john@example.com">
+              <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+              <input required  type="text" id="adr" name="address" placeholder="542 W. 15th Street">
+              <label for="city"><i class="fa fa-institution"></i> City</label>
+              <input required  type="text" id="city" name="city" placeholder="New York">
 
-		<section class="checkout-details">
-			<div class="checkout-details-inner">
-				<div class="checkout-lists">
-					<?php
-					// $id = $_Get['id'];
-					// echo $id; die();
-					$result = select("cart");
+              <div class="row">
+                <div class="col-50">
+                  <label for="state">State</label>
+                  <input required  type="text" id="state" name="state" placeholder="NY">
+                </div>
+                <div class="col-50">
+                  <label for="zip">Zip</label>
+                  <input  required type="text" id="zip" name="zip" placeholder="10001">
+                </div>
+              </div>
+            </div>
 
-					$sq = "SELECT * FROM `product` INNER JOIN `cart` ON product.`id` = cart.`id`";
-					print_r($sq) ; die();
-					$q = mysqli_query($conn, $sq);
-					 $ql = "SELECT * FROM `cart` WHERE id = '$id'";
-					$result = mysqli_query($conn , $ql);
+            <div class="col-50">
+              <h3>Payment</h3>
+              <label for="fname">Accepted Cards</label>
+              <div class="icon-container">
+                <i class="fa fa-cc-visa" style="color:navy;"></i>
+                <i class="fa fa-cc-amex" style="color:blue;"></i>
+                <i class="fa fa-cc-mastercard" style="color:red;"></i>
+                <i class="fa fa-cc-discover" style="color:orange;"></i>
+              </div>
+              <label for="cname">Name on Card</label>
+              <input type="text" id="cname" name="cardname" placeholder="John More Doe">
+              <label for="ccnum">Credit card number</label>
+              <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+              <label for="expmonth">Exp Month</label>
+              <input type="text" id="expmonth" name="expmonth" placeholder="September">
+              <div class="row">
+                <div class="col-50">
+                  <label for="expyear">Exp Year</label>
+                  <input type="text" id="expyear" name="expyear" placeholder="2018">
+                </div>
+                <div class="col-50">
+                  <label for="cvv">CVV</label>
+                  <input type="text" id="cvv" name="cvv" placeholder="352">
+                </div>
+              </div>
+            </div>
 
-					$fetch = mysqli_fetch_all($q, MYSQLI_ASSOC);
-
-					$total_bill = 0;
-					foreach($fetch as $res){ 
-						$total = [];
-						$total[] = $res
-						['price'] * $res['qty'];
-						$total_bill += $res['price'] * $res['qty'];
-						?>
-						<div class="card">
-							<div class="card-image"><img src="" alt=""></div>
-							<div class="card-details">
-								<div class="card-name">rth<h4></h4><?php echo $res['name']; ?></div>
-								<div class="card-price"><h4></h4><?php echo $res['qty']; ?></div>
-								<div class="card-price"><h4></h4><?php echo $res['price']; ?></div><div class="card-price"><h4></h4><?php echo $res['color']; ?></div>
-								<div class="card-price"><h4></h4><?php echo $total[0]; ?></div>
-
-							</div>
-						</div>
-					<?php } ?>
-				</div>
-				<div class="checkout-total">
-					<h2>Sub Total</h2>
-					<h2>$0</h2>
-				</div>
-			</div>
-		</section>
-
-	</main>
-
-	<footer>
-
-	</footer>
-	<!-- partial -->
+          </div>
+          <label>
+            <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
+          </label>
+          <input type="submit" value="Continue to checkout" class="btn">
+        </form>
+      </div>
+    </div>
+    <div class="col-25">
+      <div class="container">
+        <h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> <b>4</b></span></h4>
+        <p><a href="#">Product 1</a> <span class="price">$15</span></p>
+        <p><a href="#">Product 2</a> <span class="price">$5</span></p>
+        <p><a href="#">Product 3</a> <span class="price">$8</span></p>
+        <p><a href="#">Product 4</a> <span class="price">$2</span></p>
+        <hr>
+        <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
+      </div>
+    </div>
+  </div>
 
 </body>
 </html>
