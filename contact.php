31b1e10319c17f168633ff1dc8f0804c793e1abb
ateeq <ateeqrehman4344@gmail.com>
2022-09-19 17:36:31 -1200

    jhj

--- contact.php
+++ contact.php
@@ -6,15 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-<!--Less styles -->
-   <!-- Other Less css file //different less files has different color scheam
-	<link rel="stylesheet/less" type="text/css" href="themes/less/simplex.less">
-	<link rel="stylesheet/less" type="text/css" href="themes/less/classified.less">
-	<link rel="stylesheet/less" type="text/css" href="themes/less/amelia.less">  MOVE DOWN TO activate
-	-->
-	<!--<link rel="stylesheet/less" type="text/css" href="themes/less/bootshop.less">
-	<script src="themes/js/less.js" type="text/javascript"></script> -->
-	
+
 <!-- Bootstrap style --> 
     <link id="callCss" rel="stylesheet" href="themes/bootshop/bootstrap.min.css" media="screen"/>
     <link href="themes/css/base.css" rel="stylesheet" media="screen"/>
@@ -32,69 +24,7 @@
 	<style type="text/css" id="enject"></style>
   </head>
 <body>
-<div id="header">
 
-<!-- Navbar ================================================== -->
-<div id="logoArea" class="navbar">
-				<a id="smallScreen" data-target="#topMenu" data-toggle="collapse" class="btn btn-navbar">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</a>
-				<div class="navbar-inner">
-					<a class="brand" href="index.php">
-						<h1 class="text-success">SHOPEX</h1></a>
-						<form class="form-inline navbar-search" style="margin-left: 100px;" method="post" action="products.html" >
-							<input id="srchFld" class="srchTxt" type="text" />
-
-							<button type="submit" id="submitButton" class="btn btn-primary">Go</button>
-						</form>
-						<!-- <a href="product_summary.html" class=""><span class="btn btn-mini btn-primary mt-3"><i class="icon-shopping-cart icon-white"></i> [ 3 ] Itemes </span> </a> --> 
-
-						<ul id="topMenu" class="nav pull-right">
-							<li class=""><a href="special_offer.php">Shop</a></li>
-							<li class=""><a href="normal.html">Delivery</a></li>
-							<li class=""><a href="contact.html">Contact</a></li>
-
-
-							<li class="">
-								<a href="product_summary.html" role="button" ><span class="btn btn-large btn-primary">
-									<i class="icon-shopping-cart icon-white"></i> [ 3 ] Itemes
-								</span></a></li>
-
-
-
-								<li class="">
-									<a href="#login" role="button" data-toggle="modal" style="padding-right:0"><span class="btn btn-large btn-success">Login</span></a>
-									<div id="login" class="modal hide fade in" tabindex="-1" role="dialog" aria-labelledby="login" aria-hidden="false" >
-										<div class="modal-header">
-											<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-											<h3>Login Block</h3>
-										</div>
-										<div class="modal-body">
-											<form class="form-horizontal loginFrm">
-												<div class="control-group">								
-													<input type="text" id="inputEmail" placeholder="Email">
-												</div>
-												<div class="control-group">
-													<input type="password" id="inputPassword" placeholder="Password">
-												</div>
-												<div class="control-group">
-													<label class="checkbox">
-														<input type="checkbox"> Remember me
-													</label>
-												</div>
-											</form>		
-											<button type="submit" class="btn btn-success">Sign in</button>
-											<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-										</div>
-									</div>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div> 
 <!-- Header End====================================================================== -->
 <div id="mainBody">
 <div class="container">
